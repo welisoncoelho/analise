1c6069ba8a0e612b27a6384f72791fc502bb2020
WELISON COELHO <118991819+welisoncoelho@users.noreply.github.com>
2023-11-18 21:59:20 -0300

    Add files via upload

---
+++ Dados.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import streamlit as st
+import openpyxl as op
+
+
+st.title("Aplicativo an+alise Preços de combustivel/GLP")
+
+
+st.markdown(""" 
+            Este aplicativo foi feito de acordo com os dados semanais fornecidos pela a ANP(Agência Nacional de Petroleo)
+            no link https://www.gov.br/anp/pt-br/assuntos/precos-e-defesa-da-concorrencia/precos/levantamento-de-precos-de-combustiveis-ultimas-semanas-pesquisadas,
+            Frisamos que estes dados forom reirados para uma pequena demostração do poder do Phyton para fazer você economizar nas suas finanças. 
+            Pois com essas informações posso ver o Combustivel/GPL mais barato no meu bairro assim economizando).          
+            """)
+
+
